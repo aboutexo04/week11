7cb73dc74e5088298b54afb7563755313a9deace
Seoyeon Mun <symun@SunnysMBAir.local>
2025-04-28 11:05:47 +0900

    corrected vite.config.js file

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: 'https://aboutexo04.github.io/week10_1/',
-  plugins: [react()]
-})
+  base: "/",
+  plugins: [react()],
+});
